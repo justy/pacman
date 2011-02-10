920b3fa62e0fc81c2b55b6f65c147d163068fa98
Justy <justy@coolfusion.com.au>
2011-02-10 11:18:17 +1100

    Add resources and usage view

---
+++ views/layout.rb
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>Pacman</title>
+
+    <link rel="stylesheet" media="screen" href="/css/reset.css" />
+    <link rel="stylesheet" media="screen" href="/css/grid.css" />
+    <link rel="stylesheet" media="screen" href="/css/style.css" />
+    <link rel="stylesheet" media="screen" href="/css/messages.css" />
+    <link rel="stylesheet" media="screen" href="/css/forms.css" />
+    <link rel="stylesheet" media="screen" href="/css/baron.css" />
+    <link rel="stylesheet" media="screen" href="/css/tables.css" />
+
+    <!--[if lt IE 8]> <link rel="stylesheet" media="screen" href="/css/ie.css" /> <![endif]-->
+
+    <!-- jquerytools --> <script src="/js/jquery.tools.min.js"></script>
+
+    <script type="text/javascript" src="/js/global.js"></script>
+
+    <!--[if lt IE 9]> <script type="text/javascript" src="/js/html5.js"></script> <script type="text/javascript" src="/js/PIE.js"></script> <script type="text/javascript"
+    src="/js/IE9.js"></script> <script type="text/javascript" src="/js/ie.js"></script> <![endif]-->
+
+    <script type="text/javascript" src="/javascripts/jquery-1.4.2.js"></script>
+    <script type="text/javascript" src="/javascripts/jquery.tmpl.js"></script>
+    <script type="text/javascript" src="/javascripts/jquery.configuploader.js"></script>
+    <script type="text/javascript" src="/javascripts/jquery.appuploader.js"></script>
+    <!-- <script type="text/javascript" src="/javascripts/rails.js"></script> -->
+    <script type="text/javascript" src="/javascripts/fileuploader.js"></script>
+    <script type="text/javascript" src="/javascripts/baron.js"></script>
+
+
+  </head>
+  <body>
+    <%= yield %>
+  </body>
+
+</html>
